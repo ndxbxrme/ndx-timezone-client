0db2c7c35a681caae0d2c422498cc2d7a1094b16
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-13 07:11:59 +0000

    monday morning

--- build/index.js
+++ build/index.js
@@ -11,35 +11,37 @@
     module = angular.module('ndx-timezone-client', []);
   }
 
-  module.run(function($rootScope, $http) {
+  module.run(function($rootScope, $http, auth) {
     var reported;
     reported = false;
     return $rootScope.$on('$stateChangeSuccess', function() {
       var d, i, lastOffset, timezone;
-      timezone = [];
-      if (!reported) {
-        i = 0;
-        d = new Date(new Date().toDateString());
-        lastOffset = d.getTimezoneOffset();
-        while (i++ < 365 * 5 * 24) {
-          if (d.getTimezoneOffset() !== lastOffset) {
-            timezone.push({
-              date: d.valueOf(),
-              offset: lastOffset
-            });
-            lastOffset = d.getTimezoneOffset();
+      if (auth.getUser()) {
+        timezone = [];
+        if (!reported) {
+          i = 0;
+          d = new Date(new Date().toDateString());
+          lastOffset = d.getTimezoneOffset();
+          while (i++ < 365 * 5 * 24) {
+            if (d.getTimezoneOffset() !== lastOffset) {
+              timezone.push({
+                date: d.valueOf(),
+                offset: lastOffset
+              });
+              lastOffset = d.getTimezoneOffset();
+            }
+            d.setHours(d.getHours() + 1);
           }
-          d.setHours(d.getHours() + 1);
+          return $http.post('/api/timezone', {
+            timezone: timezone
+          }).then(function(response) {
+            if (response.data === 'OK') {
+              return reported = true;
+            }
+          }, function(err) {
+            return false;
+          });
         }
-        return $http.post('/api/timezone', {
-          timezone: timezone
-        }).then(function(response) {
-          if (response.data === 'OK') {
-            return reported = true;
-          }
-        }, function(err) {
-          return false;
-        });
       }
     });
   });
